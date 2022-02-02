0e91f05dddfd589614b4aaa0ebd363124e998b05
FasHub <myappsoftware27@gmail.com>
2022-02-02 16:52:26 +0300

    some modification

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const userRoute = require('./src/routes/user');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api/users', userRoute);
+
+
+
+
+
+app.listen(process.env.port || port, () => {
+    console.log(`Server Started on port ${port}`);
+});
